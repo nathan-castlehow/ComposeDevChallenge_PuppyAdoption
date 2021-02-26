416adde8eac4aee80d07a4301dd1356e3ef623d4
Nathan Castlehow <nathan@vitruvian.me>
2021-02-26 15:15:39 +0800

    Fixed images

--- app/src/main/java/com/example/androiddevchallenge/components/PuppyDetail.kt
+++ app/src/main/java/com/example/androiddevchallenge/components/PuppyDetail.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -15,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
@@ -31,7 +33,7 @@ import com.example.androiddevchallenge.model.Puppy
 fun PuppyDetail(currentPuppy: Puppy) {
    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize().padding(5.dp), verticalArrangement = Arrangement.SpaceBetween) {
        Column() {
-           RemoteImage(currentPuppy.imageUrl)
+           RemoteImage(currentPuppy.imageUrl, modifier = Modifier.clip(CircleShape))
            Text(currentPuppy.name, fontWeight = FontWeight.Bold, textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth())
            Text(currentPuppy.description)
        }
--- app/src/main/java/com/example/androiddevchallenge/components/PuppyListCard.kt
+++ app/src/main/java/com/example/androiddevchallenge/components/PuppyListCard.kt
@@ -7,12 +7,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
@@ -36,7 +38,7 @@ fun PuppyListCard(id:String,name: String, description:String, breed:String, imag
         .background(color = Color.White)
         .padding(10.dp), verticalAlignment = Alignment.CenterVertically) {
         Column() {
-            Row() {
+            Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
                 Column() {
                     Text(name, fontWeight = FontWeight.Bold)
                     Text(buildAnnotatedString {
@@ -47,7 +49,8 @@ fun PuppyListCard(id:String,name: String, description:String, breed:String, imag
                     })
                 }
                 Column(modifier = Modifier.height(50.dp).width(50.dp)) {
-                    RemoteImage(url = image, modifier = Modifier.fillMaxWidth().fillMaxHeight())
+                    RemoteImage(image, modifier = Modifier.clip(CircleShape))
+
                 }
             }
             Text("What to know?", fontWeight = FontWeight.Bold)
